2718f1b65bb245b67d417fbe044e317112b63840
Kevindai2369 <105852016+Kevindai2369@users.noreply.github.com>
2025-09-03 15:05:33 +0700

    fix things

--- Midterm/demo/lib/main.dart
+++ Midterm/demo/lib/main.dart
@@ -1,122 +1,210 @@
+// lib/main.dart
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // Biến để theo dõi ngôn ngữ hiện tại, mặc định là tiếng Anh
+  Locale _locale = const Locale('en', '');
+
+  void _changeLanguage(Locale locale) {
+    setState(() {
+      _locale = locale;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      title: 'Flutter i18n & l10n Demo',
+      // Cấu hình các locales được hỗ trợ, bây giờ có 3 ngôn ngữ
+      supportedLocales: const [
+        Locale('en', ''), // Tiếng Anh
+        Locale('vi', ''), // Tiếng Việt
+        Locale('ja', ''), // Tiếng Nhật
+      ],
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      locale: _locale,
+      home: MyHomePage(
+        title: 'Internationalization & Localization Demo (3 Languages)',
+        onLanguageChanged: _changeLanguage,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  const MyHomePage({
+    Key? key,
+    required this.title,
+    required this.onLanguageChanged,
+  }) : super(key: key);
 
   final String title;
+  final Function(Locale) onLanguageChanged;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _itemCount = 0;
 
-  void _incrementCounter() {
+  void _incrementItemCount() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _itemCount++;
+    });
+  }
+
+  void _decrementItemCount() {
+    setState(() {
+      if (_itemCount > 0) {
+        _itemCount--;
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final appLocalizations = AppLocalizations.of(context)!;
+    final currentLocale = Localizations.localeOf(context);
+
+    // Định dạng ngày tháng
+    final DateTime now = DateTime.now();
+    final DateFormat dateFormatter = DateFormat.yMMMMd(currentLocale.languageCode);
+    final String formattedDate = dateFormatter.format(now);
+
+    // Định dạng tiền tệ
+    final double price = 1234567.89;
+    String currencySymbol;
+    if (currentLocale.languageCode == 'vi') {
+      currencySymbol = '₫';
+    } else if (currentLocale.languageCode == 'ja') {
+      currencySymbol = '円'; // Ký hiệu Yen Nhật
+    } else {
+      currencySymbol = '\$';
+    }
+
+    final NumberFormat currencyFormatter = NumberFormat.currency(
+      locale: currentLocale.languageCode,
+      symbol: currencySymbol,
+    );
+    final String formattedCurrency = currencyFormatter.format(price);
+
+    // Định dạng số
+    final int bigNumber = 987654321;
+    final NumberFormat numberFormatter = NumberFormat.decimalPattern(currentLocale.languageCode);
+    final String formattedNumber = numberFormatter.format(bigNumber);
+
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                appLocalizations.helloWorld,
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              const SizedBox(height: 20),
+              Text(
+                appLocalizations.greeting,
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+              const SizedBox(height: 20),
+              Text(
+                appLocalizations.welcomeMessage,
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+              const SizedBox(height: 40),
+
+              Text(
+                appLocalizations.itemsInCart(_itemCount),
+                style: Theme.of(context).textTheme.headlineSmall,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: _decrementItemCount,
+                    child: const Text('-'),
+                  ),
+                  const SizedBox(width: 10),
+                  ElevatedButton(
+                    onPressed: _incrementItemCount,
+                    child: const Text('+'),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 40),
+
+              Text(
+                appLocalizations.currentDate(formattedDate),
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+              const SizedBox(height: 20),
+
+              Text(
+                appLocalizations.amount(formattedCurrency),
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+              const SizedBox(height: 20),
+
+              Text(
+                'Large Number: ${formattedNumber}',
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+              const SizedBox(height: 40),
+
+              Text(
+                appLocalizations.languageSelection, // Text "Select Language:"
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () => widget.onLanguageChanged(const Locale('en', '')),
+                    child: const Text('English'),
+                  ),
+                  const SizedBox(width: 10),
+                  ElevatedButton(
+                    onPressed: () => widget.onLanguageChanged(const Locale('vi', '')),
+                    child: const Text('Tiếng Việt'),
+                  ),
+                  const SizedBox(width: 10),
+                  ElevatedButton(
+                    onPressed: () => widget.onLanguageChanged(const Locale('ja', '')),
+                    child: const Text('日本語'), // Japanese
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
